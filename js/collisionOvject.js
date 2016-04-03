bbda4ae68396c58287b82fcb2a3088d67ade834f
DudduYum <mioemeil@gmail.com>
2016-04-03 18:10:52 +0200

    I managed to make some sort of collision detection manager, however it isn't finished yet, still need to do: 	*debagging 	*integration in global project 	*have to finished a method

---
+++ js/collisionOvject.js
@@ -0,0 +1,81 @@
+//      COLLIDERS
+
+
+//THREE.Mesh {quello che c'era prima }  ---->   THREE.Mesh{quello che c'era prima,  collider:..., rays:..., rayCaster:...}
+function preperPlayerObject(obj,rays){
+	obj.collider = addColliderToPlayerObject(obj.vertex);
+	//tmp
+	obj.collider new THREE.Sphere(obj.position, 5);
+	//end tmp
+	obj = addRayCaster(obj,rays);
+	return obj;
+}
+
+function addColliderToPlayerObject(vertex){
+	.setFromPoints (vertex, optionalCenter);
+
+//number hexNum ---->  THREE.Mesh {quello che c'era prima ,radius:...}
+function makeGameTargetObject(radius,color){
+//define mesh
+	var objGeometry = new THREE.SphereGeometry( radius, 32, 32 );
+	var objMaterial = new THREE.MeshBasicMaterial( {color:color} );
+	var objMesh = new THREE.Mesh(objGeometry  , objMaterial );
+	//define new property
+	objMesh.radius = radius;
+//define collider
+	var objMesh = addSphereCollider(objMesh);
+	//var gObj = {mesh:objMesh , radius:radius};
+	return objMesh;
+}
+//THREE.Mesh {quello che c'era prima ,radius:...}  ---->   THREE.Mesh{quello che c'era prima, collider:...}
+function addSphereCollider(obj){
+	center = obj.position;
+	radius = obj.radius;
+	objCollider = new THREE.Sphere(center,radius);
+	obj.collider = objCollider;
+	//test
+	obj.mesh.collider = objCollider;
+	return obj;
+}
+//No meteoriti per ora
+//THREE.Mesh{quello che c'era prima, collider:...}   ---->  THREE.Mesh{quello che c'era prima, array:..., rayCaster:...}
+function addRayCaster(obj,arr){
+	//add rays, rays are used by collision manager to detect object the playObject may collide with	
+	obj.rays = arr;
+	//raycater use rays to progect a vectors
+	// maybe I will delete it in the future
+	var ray = new THREE.Raycaster();
+	obj.rayCaster = ray;
+	return obj;
+}
+//obj{mesh:...,  x:..., y:..., z:...,} ---->  void
+function moveGameObj(obj,x,y,z){
+	obj.mesh.position.set(x,y,z);
+	obj.mesh.collider.center.set(x,y,z);
+}
+
+
+
+//	COLLIDER MANAGER
+
+function createCollisionManager(){
+	obj = {};
+	obj.Targets = new Array();
+	obj.Players = new Array();
+}
+
+function checkCollision(obj){
+	for(i = 0 ; i < obj.Players.length ; i++){
+		directions = obj.Players[i].rays;
+		ray = obj.Players[i].rayCaster;
+		for(j = 0 ; j < direction.length ; j++){
+			ray.set(obj.Players[i].position, direction[j]);
+			var collisions = ray.intersectObjects(obj.Targets);
+			if(collisions.length > 0){
+				if(obj.Players[i].collider.intersectsSphere(collisions[0].object.collider)){
+	//do something bro ...
+				}
+			}
+		}
+	}
+}	
