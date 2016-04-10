3b1e4b5f9f4883d28ceb525bcf10f10bd25016c1
Oni07R <oni07r@gmail.com>
2016-04-10 17:29:55 +0200

    null

--- js/rendering.js
+++ js/rendering.js
@@ -1,3 +1,5 @@
+var orbitControls = new THREE.OrbitControls(camera, renderer.domElement);
+
 animate();
 
 function animate() {
@@ -5,14 +7,14 @@ function animate() {
 		flowTime();
 		moveSpaceship(timePassed);
 		moveAsteroids(timePassed);
+		checkCollision();
+		cleanAsteroids();
 		if(clock > spawnDelay) {
  			clock = clock % spawnDelay;
  			generateAsteroid();
  			genereteAsteroidCollider();
  		}
- 		scoreNumTab.innerHTML =  gameScore;
-		cleanAsteroids();
-		checkCollision();
+ 		scoreNumTab.innerHTML = gameScore;
 	}
 	
 	requestAnimationFrame(animate);
